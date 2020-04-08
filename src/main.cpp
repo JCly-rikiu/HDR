d0949e831e85dae686fe4dc4902d4e5135da016d
JCly-rikiu <jcly.rikiu@gmail.com>
2020-04-08 19:20:06 +0800

    simple argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,13 @@ int main(int argc, char *argv[]) {
   if (image_dir.back() != '/') image_dir += "/";
   auto image_data = load_images(image_dir);
 
-  image_data = alignment(image_data, true);
+  bool skip_alignment = false;
+  if (argc > 2) {
+    std::string arg = argv[2];
+    if (arg.compare("--skip-alignment") == 0)
+      skip_alignment = true;
+  }
+  image_data = alignment(image_data, skip_alignment);
 
   auto radiance_map = hdr(image_data);
   cv::imwrite(image_dir + "raidance_map.jpg", radiance_map);
